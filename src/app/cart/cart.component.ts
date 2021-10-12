ba64da64dad40790a3d697a19393de05871decd5
rlinkcpi <randy@cpimobile.com>
2021-10-11 21:58:07 -0400

    1 file modified, 1 added

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService } from '../cart.service';
+import { Product } from '../products';
 
 @Component({
   selector: 'app-cart',
@@ -7,9 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartComponent implements OnInit {
 
-  constructor() { }
+  items: Product[] = [];
+  constructor(private cartService: CartService) { }
 
   ngOnInit() {
+    this.items = this.cartService.getItems();
   }
 
 }
